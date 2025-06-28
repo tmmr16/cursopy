956de77ae5ea925b4a4a09bd95ba6d5be9e3503a
Thiago Meza <thiago.meza.ramirez@gmail.com>
2025-06-28 12:42:42 -0300

    usar variables

---
+++ Clase 2/1.py
@@ -0,0 +1,19 @@
+print("Hola mundo desde la UNIDA")
+
+entero = 10
+flotante = 10.5
+cadena = "Hola mundo"
+bool = True
+print(entero,flotante,cadena,bool)
+print(type(entero),type(flotante),type(cadena),type(bool))
+
+print("Suma dos numeros")
+a = 10
+b = 20
+print("Suma dos numeros ", a+b)
+print("Resta dos numeros")
+print("Resta dos numeros ", a-b)
+//Comentario1
+'''varias lineas comentarios 
+varias lineas'''
+#comentario 1 linea
