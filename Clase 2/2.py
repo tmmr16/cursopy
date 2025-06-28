600938c4996571de9d2bead58efd4dffc3c5bbcc
Thiago Meza <thiago.meza.ramirez@gmail.com>
2025-06-28 12:53:03 -0300

    operadores

---
+++ Clase 2/2.py
@@ -0,0 +1,21 @@
+'''
+a == b
+a != b
+a < b
+a > b
+a <= b
+a >= b
+'''
+
+r = 100 == 100
+print(r)
+r = 100 != 100
+print(r)
+r = 100 < 90
+print(r)
+r = 100 > 90
+print(r)
+r = 100 <= 90
+print(r)
+r = 100 >= 90
+print(r)
