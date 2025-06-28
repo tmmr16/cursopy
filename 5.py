b4663534f2e4fd4241d26047ddb95d42fc1ebedd
Thiago Meza <thiago.meza.ramirez@gmail.com>
2025-06-28 13:24:44 -0300

    entrada

---
+++ 5.py
@@ -0,0 +1,8 @@
+#Entrada de datos por teclado
+
+nombre = input("Ingresa su nombre: ")
+apellido = input("Ingrese su apellido: ")
+print(f"Nombre {nombre}, Apellido {apellido}")
+
+edad = int(input("Ingrese su edad: "))
+print(f"Tu edad es: {edad + 1}")
