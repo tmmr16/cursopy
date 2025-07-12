3fafc672f361f86b79567e093dc4c2b641a36a43
Thiago Meza <thiago.meza.ramirez@gmail.com>
2025-07-12 13:38:01 -0300

    Hola desde la UNIDA

---
+++ Clase 4/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+
+def unida():
+    return "Hola desde la unida"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host="0.0.0.0")
