6df64ee2fbda2d92d1ed9ba72d09dd4093e1bf76
Thiago Meza <thiago.meza.ramirez@gmail.com>
2025-07-12 12:46:11 -0300

    for

---
+++ Clase 3/for.py
@@ -0,0 +1,10 @@
+'''Bucle for'''
+for i in range(10):
+    print("El valor de i es ", i)
+
+lista = [1,2,3,4]
+
+for x in range(len(lista)):
+    print(lista[x])
+
+print("Fin del bucle for de la lista")
